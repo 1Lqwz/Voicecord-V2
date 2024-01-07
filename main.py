35dfbc6fc6a2bc3d91bc4daae27f5ade0bd621e7
Moha <75210277+1Lqwz@users.noreply.github.com>
2024-01-07 18:03:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ import requests
 import websocket
 from keep_alive import keep_alive
 
-status = "online" #online/dnd/idle
+status = "dnd" #online/dnd/idle
 
-GUILD_ID = ADD_YOUR_SERVER_ID_HERE
-CHANNEL_ID = ADD_YOUR_CHANNEL_ID_HERE
+GUILD_ID = 768094490119635025
+CHANNEL_ID = 1157443173627465769
 SELF_MUTE = True
-SELF_DEAF = False
+SELF_DEAF = True
 
 usertoken = os.getenv("TOKEN")
 if not usertoken:
